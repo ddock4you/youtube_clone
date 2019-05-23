65bdcfe27f320a5a78cc45c4e115fcbe860d6f9e
ddock4you <ddock4you@gmail.com>
2019-05-23 23:01:55 +0900

    라우터 세부적으로 한 번 더 나눔. 퍼그 배움

--- app.js
+++ app.js
@@ -4,30 +4,25 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import { userRouter } from './router';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
+import globalRouter from './routers/globalRouter';
+import routes from './routes';
 
 const app = express();
 
-const handleHome = (req, res) => {
-    res.send('Hello from home');
-}
 
-const handleProfile = (req, res) => {
-    res.send('You are on my profile');
-}
-
+app.set('view engine', 'pug');
 // middleware setting
 // morgan: 서버 상태, 페이지 이동 했을 때 간략한 정보 제공(접속자 브라우저 정보 등)
-app.use(morgan('dev')); //other option: tiny, combine
 // node.js 보안 앱
-app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(helmet());
+app.use(morgan('dev')); //other option: tiny, combine
 
-// 루트에 접속했을 경우
-app.get('/', handleHome);
-app.get('/profile', handleProfile);
-app.use('/use', userRouter);
-
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 export default app;
