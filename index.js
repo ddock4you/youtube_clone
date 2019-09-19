28d8d618aece4421a9002e0be361764e226a2087
ddock4you <ddock4you@gmail.com>
2019-09-19 23:33:58 +0900

    - get, request, response 기본 개념 확립 - 간단한 라우터 작동 원리 확인

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ const handleListening = () => {
     console.log(`Listening http://localhost:${PORT}`);
 }
 
+const handleHome = (req, res) => {
+    console.log(req);
+    res.send('Home');
+}
+
+const handleProfile = (req, res) => {
+    res.send('Profile');
+}
+
+
+app.get('/', handleHome);
+app.get('/profile', handleProfile);
+
 app.listen(PORT, handleListening);
