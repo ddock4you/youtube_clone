4225de44021172ed756fc816f6ec0777597c5e21
ddock4you <ddock4you@gmail.com>
2019-09-22 00:11:07 +0900

    - router 세분화(video, user, global)

---
+++ routers/videoRouter.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const videoRouter = express.Router();
+
+videoRouter.get('/', (req, res) => res.send('video index'));
+videoRouter.get('/edit', (req, res) => res.send('video edit'));
+videoRouter.get('/password', (req, res) => res.send('video password'));
+
+export default videoRouter;
