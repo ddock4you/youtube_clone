e18317b8e2c68f1c27b8683d95445df1f4ee545f
ddock4you <ddock4you@gmail.com>
2019-09-22 00:57:58 +0900

    -route 관리 파일 생성 ( routes.js)

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import routes from '../routes';
 
 const userRouter = express.Router();
 
-userRouter.get('/', (req, res) => res.send('user index'));
-userRouter.get('/edit', (req, res) => res.send('user edit'));
-userRouter.get('/password', (req, res) => res.send('user password'));
+userRouter.get(routes.home, (req, res) => res.send('user index'));
+userRouter.get(routes.userDetail, (req, res) => res.send('user detail'));
+userRouter.get(routes.editProfile, (req, res) => res.send('user edit'));
+userRouter.get(routes.changePassword, (req, res) => res.send('user password'));
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import routes from '../routes';
 
 const videoRouter = express.Router();
 
-videoRouter.get('/', (req, res) => res.send('video index'));
-videoRouter.get('/edit', (req, res) => res.send('video edit'));
-videoRouter.get('/password', (req, res) => res.send('video password'));
+videoRouter.get(routes.home, (req, res) => res.send('video'));
+videoRouter.get(routes.upload, (req, res) => res.send('video upload'));
+videoRouter.get(routes.videoDetail, (req, res) => res.send('video videoDetail'));
+videoRouter.get(routes.editVideo, (req, res) => res.send('video editVideo'));
+videoRouter.get(routes.deleteVideo, (req, res) => res.send('video deleteVideo'));
 
 export default videoRouter;
