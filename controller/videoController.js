de7506c0e2b1b219f56f057867feb3fd308f4d15
ddock4you <ddock4you@gmail.com>
2019-09-27 23:11:30 +0900

    - 검색영역 생성 - 검색창에 입력한 값을 검색 페이지로 값을 넘겨주기

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,5 +1,12 @@
 export const home = (req, res) => res.render('home', {pageTitle: 'home'});
-export const search = (req, res) => res.render('search', {pageTitle: 'search'});
+export const search = (req, res) => {
+    const {
+        query: { term: searchingBy }
+    } = req
+    // console.log(searchingBy);
+    res.render('search', {pageTitle: 'search', searchingBy});
+}
+
 export const videos = (req, res) => res.render('videos', {pageTitle: 'videos'});
 export const upload = (req, res) => res.render('upload', {pageTitle: 'upload'});
 export const videoDetail = (req, res) => res.render('videoDetail', {pageTitle: videoDetail});
