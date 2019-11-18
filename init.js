f8c651d8c89bd3a7b08430cfda702ead83ed0cba
ddock4you <ddock4you@gmail.com>
2019-11-18 15:59:56 +0900

    - github 연동 중..(로그인 성공하고 계정 정보 받는 것 까지 적용 - 로그아웃 기능 수정

--- init.js
+++ init.js
@@ -1,11 +1,12 @@
 import dotenv from "dotenv";
 import "./db";
 import app from './app';
-import "./models/Video.js";
-import "./models/Comment.js";
+
 dotenv.config();
 
-
+import "./models/Video";
+import "./models/Comment";
+import "./models/User";
 
 
 const PORT = process.env.PORT || 4000;
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import passport from 'passport';
 import routes from '../routes';
 import { home, search } from '../controller/videoController';
-import { logout, getJoin, postJoin, getLogin, postLogin } from '../controller/userController';
-import { onlyPublic } from '../middlewares';
+import { logout, getJoin, postJoin, getLogin, postLogin, postGithubLogin, githubLogin } from '../controller/userController';
+import { onlyPublic, onlyPrivate } from '../middlewares';
 
 const globalRouter =  express.Router();
 
@@ -13,8 +14,13 @@ globalRouter.post(routes.join, onlyPublic, postJoin ,postLogin);
 globalRouter.get(routes.login, onlyPublic, getLogin);
 globalRouter.post(routes.login, onlyPublic, postLogin);
 
+globalRouter.get(routes.logout, onlyPrivate, logout);
 
-globalRouter.get(routes.logout, onlyPublic, logout);
+globalRouter.get(routes.github, githubLogin);
+
+globalRouter.get(routes.githubCallback, passport.authenticate("github", { failureRedirect: "/login" }),
+postGithubLogin)
+
 globalRouter.get(routes.search, search);
 
 export default globalRouter;
