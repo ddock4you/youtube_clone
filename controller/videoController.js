71cf2786f879e5308c0b0cf69516fd27ad9389b1
ddock4you <ddock4you@gmail.com>
2019-11-29 23:50:24 +0900

    - video 업로드 및 삭제 보안 부분 좀 더 강화 - 다른 유저를 클릭했을 때 그 유저가 업로드 한 영상 확인 가능하도록 수정

--- controller/videoController.js
+++ controller/videoController.js
@@ -74,7 +74,11 @@ export const getEditVideo =async (req, res) => {
     } = req;
     try {
         const video = await Video.findById(id);
-        res.render('editVideo', {pageTitle: 'editVideo', video});
+        if (video.creator !== req.user.id) {
+            throw Error();
+        } else {
+            res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+        }
     } catch(error) {
         res.redirect(routes.home);
 
@@ -102,7 +106,12 @@ export const deleteVideo = async (req, res) => {
     } = req;
 
     try {
-        await Video.findOneAndRemove({ _id: id });
+        const video = await Video.findById(id);
+        if (video.creator !== req.user.id) {
+            throw Error();
+        } else {
+            await Video.findOneAndRemove({ _id: id });
+        }
     } catch (error) {
 
     }
