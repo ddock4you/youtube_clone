73f0e4b93a8514331b99ad9409969a2cf0250dde
ddock4you <ddock4you@gmail.com>
2020-06-15 23:54:30 +0900

    - middleware 이해

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ const handleListening = () => {
 };
 
 app.listen(PORT, handleListening);
+
+const handleHome = (req, res) => res.send("Hello");
+
+const handleProfile = (req, res) => res.send("Profile");
+
+const betweenHome = (req, res, next) => {
+    console.log("Beetween");
+    next();
+};
+
+app.use(betweenHome); // 순서 중요
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
